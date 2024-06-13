c7e0d04b4d75f71ad9430b88f1afee25327bfdd1
Je4nComJ <jeanvictor.fa@gmail.com>
2024-06-13 12:07:49 -0300

    projeto criado

---
+++ backend/src/main/java/com/gomesdevelopers/jvvendas/JvvendasApplication.java
@@ -0,0 +1,13 @@
+package com.gomesdevelopers.jvvendas;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class JvvendasApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(JvvendasApplication.class, args);
+	}
+
+}
---
+++ backend/src/test/java/com/gomesdevelopers/jvvendas/JvvendasApplicationTests.java
@@ -0,0 +1,13 @@
+package com.gomesdevelopers.jvvendas;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class JvvendasApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
