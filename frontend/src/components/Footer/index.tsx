cf2c36547e15340a92f816274fd94d88a4a77eea
Je4nComJ <jeanvictor.fa@gmail.com>
2024-06-14 14:16:32 -0300

    Basic static components

---
+++ frontend/src/components/Footer/index.tsx
@@ -0,0 +1,13 @@
+const Footer = () => {
+    return (
+   
+    <footer className="footer mt-auto py-3 bg-dark">
+        <div className="container">
+            <p className="text-light">App desenvolvido por <a href="https://github.com/Je4nComJ" target="_blank" rel="noreferrer">Jean Victor</a></p>
+        </div>
+    </footer>
+    );
+  }
+  
+  export default Footer;
+  
